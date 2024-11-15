b8ff9cf167674f3c41fef7770356eb600eb53643
Le Van Quy <vannn.quy@gmail.com>
2024-11-15 17:53:27 +0700

    up

--- src/components/Section/BannerHomeSilder.jsx
+++ src/components/Section/BannerHomeSilder.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import iconSearch from "../../assets/icons/search.svg";
 import { IoLocation } from "react-icons/io5";
 import { RiPriceTagFill } from "react-icons/ri";
@@ -9,9 +9,83 @@ import supBanner2 from "../../assets/images/sup2-banner.png";
 import supBanner3 from "../../assets/images/sup3-banner.jpg";
 import supBanner4 from "../../assets/images/sup4-banner.jpg";
 import supBanner5 from "../../assets/images/sup5-banner.jpg";
+import { getAllDistrict, getAllProvince } from "../../services/api/SuperShipService";
+import { useQuery } from "react-query";
+import Select from 'react-select';
 
 const BannerHomeSilder = () => {
   const images = [supBanner1, supBanner2, supBanner3, supBanner4, supBanner5];
+  const [selectedProvince, setSelectedProvince] = useState(null);
+  const [selectedDistrict, setSelectedDistrict] = useState(null);
+  const { data: provinces } = useQuery({
+    queryKey: ['provinces'],
+    queryFn: () => getAllProvince(),
+  });
+
+  const { data: districts, isLoading: isLoadingDistricts } = useQuery(
+    ['districts', selectedProvince?.code],
+    () => getAllDistrict(selectedProvince?.code),
+    {
+      enabled: !!selectedProvince,
+    }
+  );
+  console.log(districts)
+  console.log(selectedProvince?.code)
+
+  const handleProvinceChange = (selectedOption) => {
+    setSelectedProvince(selectedOption);
+    setSelectedDistrict(null);
+    setDistrictOptions(districts[selectedOption.value] || []);
+    setCode(selectedOption.value);
+  };
+
+  // Xử lý khi quận huyện thay đổi
+  const handleDistrictChange = (selectedOption) => {
+    setSelectedDistrict(selectedOption);
+  };
+
+
+  const customStyles = {
+    control: (provided) => ({
+      ...provided,
+      backgroundColor: 'inherit', // Màu nền ăn theo màu của thẻ cha
+      borderColor: '#ccc', // Thêm border color nếu cần
+      padding: '0',
+      fontSize: '16px',
+      borderRadius: '8px',
+      border: "none",
+      boxShadow: 'none', // Loại bỏ shadow (nếu có)
+    }),
+    menu: (provided) => ({
+      ...provided,
+      backgroundColor: 'inherit', // Màu nền menu ăn theo thẻ cha
+      boxShadow: '0px 4px 12px rgba(0, 0, 0, 0.1)', // Thêm bóng cho menu
+      borderRadius: '8px',
+    }),
+    option: (provided, state) => ({
+      ...provided,
+      backgroundColor: state.isSelected ? '#ddd' : '#fff', // Thay đổi màu khi option được chọn
+      color: state.isSelected ? '#000' : '#333',
+      padding: '8px',
+      borderRadius: '4px',
+      cursor: 'pointer',
+    }),
+    dropdownIndicator: (provided) => ({
+      ...provided,
+      display: 'none', // Ẩn mũi tên xuống
+    }),
+    indicatorSeparator: (provided) => ({
+      ...provided,
+      display: 'none', // Ẩn đường phân cách giữa icon và ô chọn
+    }),
+    placeholder: (provided) => ({
+      ...provided,
+      color: '#888', // Thay đổi màu chữ của placeholder
+    }),
+  };
+
+
+
   return (
     <section className="relative p-8 bg-main-home rounded-xl h-[80vh]">
       <div className="flex gap-8">
@@ -37,7 +111,7 @@ const BannerHomeSilder = () => {
         </div>
       </div>
       <div className="absolute left-8 right-8 -bottom-[8vh] p-6 bg-white shadow-black rounded-xl shadow-2xl">
-        <ul className="flex gap-2 font-medium uppercase py-2 overflow-x-auto">
+        <ul className="flex font-medium uppercase py-2 overflow-x-auto">
           <li className="flex items-center gap-2 flex-1 whitespace-nowrap px-4 min-w-96">
             <IoLocation size={24} /> Khu vực/ Địa điểm
           </li>
@@ -53,16 +127,29 @@ const BannerHomeSilder = () => {
           <div className="bg-[#e1e1e1] flex flex-1 px-3 py-2 rounded gap-4 min-w-96">
             <label className="flex flex-col w-full" htmlFor="">
               <span className="text-zinc-600">Chọn tỉnh thành </span>
-              <input
-                className="p-1 outline-none border-none bg-[#e1e1e1]"
-                type="text"
+              <Select
+                value={selectedProvince}
+                onChange={handleProvinceChange}
+                options={provinces}
+                styles={customStyles}
+                getOptionLabel={(e) => e.name}
+                getOptionValue={(e) => e.code}
+                menuPortalTarget={document.body}
+                placeholder='Chọn tỉnh thành'
               />
             </label>
             <label className="flex flex-col w-full" htmlFor="">
               <span className="text-zinc-600">Quận/ Huyện</span>
-              <input
-                className="p-1 outline-none border-none bg-[#e1e1e1]"
-                type="text"
+              <Select
+                options={districts}
+                value={selectedDistrict}
+                onChange={handleDistrictChange}
+                menuPortalTarget={document.body}
+                styles={customStyles}
+                getOptionLabel={(e) => e.name}
+                getOptionValue={(e) => e.code}
+                placeholder="Chọn quận huyện"
+                isDisabled={!selectedProvince}  // Disable quận huyện nếu chưa chọn tỉnh thành
               />
             </label>
           </div>
@@ -73,7 +160,7 @@ const BannerHomeSilder = () => {
           >
             <span className="text-zinc-600">Giá từ</span>
             <input
-              className="p-1 outline-none border-none bg-[#e1e1e1]"
+              className="p-1 outline-none border-none bg-[#e1e1e1] leading-7 text-base"
               type="text"
             />
           </label>
@@ -84,11 +171,11 @@ const BannerHomeSilder = () => {
           >
             <span className="text-zinc-600">Số lượng </span>
             <input
-              className="p-1 outline-none border-none bg-[#e1e1e1]"
+              className="p-1 outline-none border-none bg-[#e1e1e1] leading-7 text-base"
               type="text"
             />
           </label>
-          <button className="px-4 flex justify-center items-center text-white bg-primary rounded border-none">
+          <button className="px-5 flex justify-center items-center text-white bg-primary rounded border-none cursor-pointer">
             <img src={iconSearch} alt="" />
           </button>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { QueryClient, QueryClientProvider } from 'react-query';
 import App from './App.jsx'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
     <ToastContainer />
   </React.StrictMode>,
 )
