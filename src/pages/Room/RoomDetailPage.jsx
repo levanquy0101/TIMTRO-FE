ea7ab76f9f36256600c7ee2c522d06b0d3390338
Le Van Quy <vannn.quy@gmail.com>
2024-12-22 17:29:08 +0700

    Add API Thue Phong

--- src/pages/Room/RoomDetailPage.jsx
+++ src/pages/Room/RoomDetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Slider from 'react-slick';
 import bgPlaceholder from '../../assets/images/background-xam.jpg';
 import item1 from '../../assets/icons/area-icon.svg'
@@ -13,13 +13,67 @@ import item9 from '../../assets/icons/washing-icon.svg'
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 import { getById } from '../../services/api/RoomService';
+import { useForm } from 'react-hook-form';
+import { RentalManagementService } from '../../services/api';
 
 function RoomDetailPage(props) {
+    const [user, setUser] = useState(null);
     const { id } = useParams();
     const { data: room } = useQuery({
         queryKey: ['room', id],
         queryFn: () => getById(id),
     });
+
+    const { register, handleSubmit, formState: { errors } } = useForm();
+
+    useEffect(() => {
+        // Lấy dữ liệu từ localStorage
+        const authData = localStorage.getItem('authData');
+      
+        if (authData) {
+          const parsedAuthData = JSON.parse(authData);
+          setUser(parsedAuthData.user);
+        }
+      }, []);
+
+      const onSubmit = (data) => {
+        try {
+            if (user?.id && data) {
+                data.tenant_id = user.id;
+                data.room_id = room.id;
+                
+                const startDate = new Date(data.start_date);
+                let endDate = new Date(startDate);
+                
+                // Thêm ngày dựa trên hợp đồng
+                switch (data.end_date) {
+                    case '1':
+                        endDate.setMonth(startDate.getMonth() + 1);
+                        break;
+                    case '6':
+                        endDate.setMonth(startDate.getMonth() + 6);
+                        break;
+                    case '12':
+                        endDate.setFullYear(startDate.getFullYear() + 1);
+                        break;
+                    case '':
+                        // Không có thay đổi cho 'Chọn Sau'
+                        break;
+                    default:
+                        break;
+                }
+    
+                data.end_date = endDate.toISOString().split('T')[0]; // Chuyển đổi ngày thành định dạng YYYY-MM-DD
+                const res = RentalManagementService.create(data);
+                console.log(res);
+                console.log(data);
+            }
+        } catch (error) {
+            console.error(error);
+        }
+    };
+    
+  
 
 
     const settings = {
@@ -47,14 +101,13 @@ function RoomDetailPage(props) {
                 ))}
             </Slider>
 
-
             <section className='px-12'>
                 <div className='relative flex gap-4'>
                     <div>
                         <strong className='text-3xl block'>{room?.price} vnd/tháng</strong>
                         <strong className='uppercase text-[2rem]'>{room?.name}</strong>
                         <p>{room?.address}</p>
-                        <p>1 giường - 1 nhà vệ sinh</p>
+                        <p>{room?.bedrooms} giường - {room?.toilets} nhà vệ sinh</p>
                         <big>Quản lý bởi Mr Tứ</big>
                         <strong className='block mt-4'>Mô tả</strong>
                         <p className='leading-6'>
@@ -62,32 +115,33 @@ function RoomDetailPage(props) {
                         </p>
                     </div>
                     <div className='-mt-20 border-solid border-zinc-400 border-2 p-3 bg-white rounded min-w-96 max-w-min ml-auto'>
-                        <form action="" className='flex flex-col gap-2 mt-8'>
-                            <div className='flex gap-2 w-full'>
-                                <input className='w-1/2 outline-none p-2' type="text" placeholder='Tên' />
-                                <input className='w-1/2 outline-none p-2' type="text" placeholder='Liên hệ' />
-                            </div>
-                            <input className='outline-none p-2' type="text" placeholder='Email' />
-                            <textarea className='outline-none p-2 min-w-full' rows="4" type="text" placeholder='Lời nhắn' />
+                        <form onSubmit={handleSubmit(onSubmit)} className='flex flex-col gap-4 mt-8'>
+                            <label>
+                                <strong>Người thuê</strong>
+                                <input className='w-full p-2 outline-none' {...register("tenant_id")} disabled defaultValue={user?.name} />
+                                <p className='text-red-500'>{errors.tenant_id?.message}</p>
+                            </label>
+                            <label className='flex gap-1 whitespace-nowrap flex-col' >
+                                <strong>Ngày chuyển đến</strong>
+                                <input type="date" className='w-full p-2 outline-none' {...register("start_date", { required: "Trường là bắt buộc!" })} min={new Date().toISOString().split('T')[0]} />
+                                <small className='text-red-500'>{errors.start_date?.message}</small>
+                            </label>
                             <strong>Hợp đồng</strong>
                             <div className='flex gap-4'>
-                                <label className='flex gap-1 whitespace-nowrap' htmlFor="">
-                                    <input type="radio" />
+                                <label className='flex gap-1 whitespace-nowrap' >
+                                    <input type="radio" value={1} {...register("end_date", { required: "Trường là bắt buộc!" })} />
                                     <b >1 Tháng</b>
                                 </label>
-                                <label className='flex gap-2 whitespace-nowrap' htmlFor="">
-                                    <input type="radio" />
+                                <label className='flex gap-2 whitespace-nowrap' >
+                                    <input type="radio" value={6} {...register("end_date", { required: "Trường là bắt buộc!" })} />
                                     <b >6 Tháng</b>
                                 </label>
-                                <label className='flex gap-2 whitespace-nowrap' htmlFor="">
-                                    <input type="radio" />
+                                <label className='flex gap-2 whitespace-nowrap' >
+                                    <input type="radio" value={12} {...register("end_date", { required: "Trường là bắt buộc!" })} />
                                     <b >1 Năm</b>
                                 </label>
-                                <label className='flex gap-2 whitespace-nowrap' htmlFor="">
-                                    <input type="radio" />
-                                    <b >Chọn Sau</b>
-                                </label>
                             </div>
+                            <small className='text-red-500'>{errors.end_date?.message}</small>
                             <p>Điều khoản: Đối với thuê trọ thời hạn hợp đồng tối thiểu là 1 tháng. Nếu bạn không đồng ý vui lòng không đặt phòng để tránh xảy ra tranh chấp giữa các bên. Với hợp đồng thời hạn dài sẽ được nhiều ưu đãi về giá.</p>
                             <button className='py-2 px-3 rounded bg-primary text-white cursor-pointer border-none font-bold text-base w-max ml-auto'>Thuê phòng</button>
                         </form>
