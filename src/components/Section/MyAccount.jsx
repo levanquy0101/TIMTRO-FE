949e376a5365ca6f349b9e38fcda34c8b3defaef
Le Van Quy <vannn.quy@gmail.com>
2024-12-26 14:11:15 +0700

    Update 3 26/12/2024

--- src/components/Section/MyAccount.jsx
+++ src/components/Section/MyAccount.jsx
@@ -1,11 +1,10 @@
-import React, { useEffect, useState } from 'react';
-import { FaUser } from "react-icons/fa";
+import React, { useState } from 'react'
 import { useAuth } from '../../hooks/useAuth';
-import { useNavigate, useSearchParams } from 'react-router-dom';
+import { useQuery } from 'react-query';
+import { getAllByUser } from '../../services/api/RoomService';
 
-function MyAccount(props) {
-    const [searchParams] = useSearchParams();
-    const [positionNum, setPositionNum] = useState(1);
+
+export function AccountMe() {
     const { authData } = useAuth();
     const [isEditingName, setIsEditingName] = useState(false),
         [isEditingEmail, setIsEditingEmail] = useState(false),
@@ -16,118 +15,98 @@ function MyAccount(props) {
         [email, setEmail] = useState(authData?.user?.email),
         [phone, setPhone] = useState(authData?.user?.phone),
         [dateOfBirth, setDateOfBirth] = useState(authData?.user?.date_of_birth);
-
-    const navigate = useNavigate()
-    useEffect(() => {
-        if (!authData?.user) {
-            navigate('/');
-        }
-    }, []);
-    useEffect(() => {
-        // Lấy giá trị từ URL tham số, ví dụ: ?position=5
-        const position = searchParams.get("position");
-        if (position) {
-            setPositionNum(Number(position)); // Chuyển thành số nếu cần
-        }
-    }, [searchParams, positionNum]);
     return (
-        <section className='border-0 border-t border-solid border-zinc-400 flex ' style={{ minHeight: 'calc(100svh - 2.35rem)' }}>
-            <aside className='bg-zinc-100 max-w-96 border-0 border-r border-solid border-zinc-400'>
-                <div className='border-0 border-b border-solid border-zinc-400 text-center p-4'>
-                    <FaUser size={32} color='blue' />
-                    <h4 className='text-primary text-xl'>{authData?.user?.name}</h4>
-                </div>
-                <div className=''>
-                    <h3
-                        onClick={() => navigate("?position=1")}
-                        className={`border-0 border-b border-solid font-normal hover:underline cursor-pointer py-3 px-6 border-zinc-400 ${positionNum === 1 ? "underline" : ""
-                            }`}
-                    >
-                        Thông tin tài khoản
-                    </h3>
-                    <h3
-                        onClick={() => navigate("?position=2")}
-                        className={`border-0 border-b border-solid font-normal hover:underline cursor-pointer py-3 px-6 border-zinc-400 ${positionNum === 2 ? "underline" : ""
-                            }`}
-                    >
-                        Trọ của tôi
-                    </h3>
+        <>
+            <h2 className='mb-8 text-zinc-600'>Thông tin</h2>
+            <form className='w-1/2'>
+                <b className="text-zinc-600">Tên tài khoản</b>
+                <div className="my-2 flex gap-2 items-center">
+                    {isEditingName ? <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{name}</p>}
+                    <span className="text-primary cursor-pointer" onClick={() => setIsEditingName(!isEditingName)}>{isEditingName ? 'Save' : 'Edit'}</span>
                 </div>
 
-            </aside>
-            <div className='p-8 flex-1 min-h-[82vh]'>
-                {
-                    positionNum === 1 &&
-                    <>
-                        <h2 className='mb-8 text-zinc-600'>Thông tin</h2>
-                        <form className='w-1/2'>
-                            <b className="text-zinc-600">Tên tài khoản</b>
-                            <div className="my-2 flex gap-2 items-center">
-                                {isEditingName ? <input type="text" value={name} onChange={(e) => setName(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{name}</p>}
-                                <span className="text-primary cursor-pointer" onClick={() => setIsEditingName(!isEditingName)}>{isEditingName ? 'Save' : 'Edit'}</span>
-                            </div>
+                <b className="text-zinc-600">Email</b>
+                <div className="my-2 flex gap-2 items-center">
+                    {isEditingEmail ? <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{email}</p>}
+                    <span className="text-primary cursor-pointer" onClick={() => setIsEditingEmail(!isEditingEmail)}>{isEditingEmail ? 'Save' : 'Edit'}</span>
+                </div>
 
-                            <b className="text-zinc-600">Email</b>
-                            <div className="my-2 flex gap-2 items-center">
-                                {isEditingEmail ? <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{email}</p>}
-                                <span className="text-primary cursor-pointer" onClick={() => setIsEditingEmail(!isEditingEmail)}>{isEditingEmail ? 'Save' : 'Edit'}</span>
-                            </div>
+                <b className="text-zinc-600">Số điện thoại</b>
+                <div className="my-2 flex gap-2 items-center">
+                    {isEditingPhone ? <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{phone}</p>}
+                    <span className="text-primary cursor-pointer" onClick={() => setIsEditingPhone(!isEditingPhone)}>{isEditingPhone ? 'Save' : 'Edit'}</span>
+                </div>
 
-                            <b className="text-zinc-600">Số điện thoại</b>
-                            <div className="my-2 flex gap-2 items-center">
-                                {isEditingPhone ? <input type="text" value={phone} onChange={(e) => setPhone(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{phone}</p>}
-                                <span className="text-primary cursor-pointer" onClick={() => setIsEditingPhone(!isEditingPhone)}>{isEditingPhone ? 'Save' : 'Edit'}</span>
-                            </div>
+                <b className="text-zinc-600">Ngày sinh</b>
+                <div className="my-2 flex gap-2 items-center">
+                    {isEditingDate ? <input type="date" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{dateOfBirth}</p>}
+                    <span className="text-primary cursor-pointer" onClick={() => setIsEditingDate(!isEditingDate)}>{isEditingDate ? 'Save' : 'Edit'}</span>
+                </div>
+                <b className="text-zinc-600">Vai trò</b>
+                <div>
+                    <p className='py-2'>{authData?.user?.role}</p>
+                </div>
+            </form>
+        </>
+    )
+}
 
-                            <b className="text-zinc-600">Ngày sinh</b>
-                            <div className="my-2 flex gap-2 items-center">
-                                {isEditingDate ? <input type="date" value={dateOfBirth} onChange={(e) => setDateOfBirth(e.target.value)} className="border p-2.5 rounded w-full outline-none" /> : <p className='w-full py-2'>{dateOfBirth}</p>}
-                                <span className="text-primary cursor-pointer" onClick={() => setIsEditingDate(!isEditingDate)}>{isEditingDate ? 'Save' : 'Edit'}</span>
-                            </div>
-                            <b className="text-zinc-600">Vai trò</b>
-                            <div>
-                                <p className='py-2'>{authData?.user?.role}</p>
-                            </div>
-                        </form>
+export function RoomMe() {
+    const { data: roomsMe } = useQuery({
+        queryKey: ['room-me'],
+        queryFn: () => getAllByUser(),
+    });
 
-                    </>
-                }
-                {
-                    positionNum === 2 &&
-                    <>
-                        <h2 className='mb-8 text-zinc-600'>Trọ của tôi</h2>
-                        <h2 className='uppercase'>Tên phòng: Phòng A101</h2>
+    if (!roomsMe) {
+        return null;
+    }
+
+    return (
+        <>
+            <h2 className='mb-8 text-zinc-600'>Trọ của tôi</h2>
+            {roomsMe.map((roomData, index) => {
+                const room = roomData.room;
+                const rentalBill = roomData.rentalBills[0]; // Giả sử chỉ lấy hóa đơn đầu tiên
+
+                return (
+                    <div key={index} className="mb-8">
+                        <h3 className='uppercase'>{`Tên phòng: ${room.name}`}</h3>
                         <div className='flex flex-col gap-2 my-4'>
                             <div className='flex gap-4'>
                                 <b>Địa chỉ:</b>
-                                <p className='text-zinc-600'>09 An Thượng 09, Ngũ Hành Sơn, Đà Nẵng</p>
+                                <p className='text-zinc-600'>{room.address}</p>
                             </div>
                             <div className='flex gap-4'>
                                 <b>Giá thuê:</b>
-                                <p className='text-zinc-600'>3.000.000 VNĐ</p>
+                                <p className='text-zinc-600'>{room.price} VNĐ</p>
                             </div>
                             <div className='flex gap-4'>
                                 <b>Ngày bắt đầu hợp đồng:</b>
-                                <p className='text-zinc-600'>16/09/2023</p>
+                                <p className='text-zinc-600'>{rentalBill?.start_date}</p>
                             </div>
                             <div className='flex gap-4'>
                                 <b>Ngày kết thúc hợp đồng:</b>
-                                <p className='text-zinc-600'>16/09/2024</p>
+                                <p className='text-zinc-600'>{rentalBill?.end_date}</p>
                             </div>
                             <div className='flex flex-col gap-4 bg-zinc-200 w-1/2 p-2 rounded'>
-                                <strong > Chi phí tháng 12</strong>
-                                <strong >Tiền điện</strong>
-                                <strong >Tiền nước</strong>
-                                <strong >Tiền phòng</strong>
-                                <strong >Tổng: </strong>
+                                <strong>Chi phí tháng 12</strong>
+                                <strong>Tiền điện: {rentalBill?.electricity_usage} kWh</strong>
+                                <strong>Tiền nước: {rentalBill?.water_usage} m³</strong>
+                                <strong>Tiền phòng: {room.price} VNĐ</strong>
+                                <strong>Tổng: {calculateTotal(room, rentalBill)} VNĐ</strong>
                             </div>
                         </div>
-
-                    </>
-                }
-            </div>
-        </section>
+                    </div>
+                );
+            })}
+        </>
     );
 }
 
-export default MyAccount;
+// Hàm tính tổng chi phí
+function calculateTotal(room, rentalBill) {
+    const electricityCost = rentalBill?.electricity_usage * room.electricity_rate;
+    const waterCost = rentalBill?.water_usage * room.water_rate;
+    const rent = parseFloat(room.price);
+    return electricityCost + waterCost + rent;
+}
